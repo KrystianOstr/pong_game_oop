b84efd99478fd6909878e288d21ee60e5e583085
Krystian <krystian.ostrogorski@gmail.com>
2025-08-13 16:46:28 +0200

    prepared workflow

--- game_classes/sound_manager.py
+++ game_classes/sound_manager.py
@@ -1,11 +1,12 @@
 import pygame as py
+from settings import resource_path
 
 
 class SoundManager:
     def __init__(self):
-        self.bounce_sound = "assets/sounds/boing.wav"
-        self.point_sound = "assets/sounds/point.wav"
-        self.gameover_sound = "assets/sounds/game_end.wav"
+        self.bounce_sound = resource_path("assets/sounds/boing.wav")
+        self.point_sound = resource_path("assets/sounds/point.wav")
+        self.gameover_sound = resource_path("assets/sounds/game_end.wav")
 
     def play_bounce(self):
         sound = py.mixer.Sound(self.bounce_sound)
